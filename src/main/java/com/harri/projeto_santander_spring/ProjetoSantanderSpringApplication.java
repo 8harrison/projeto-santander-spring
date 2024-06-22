e296920defe2259c1ba2f1c47d3d2a39ba35975b
8harrison <monteiro4100@gmail.com>
2024-06-22 13:02:52 -0300

    Initial commit

---
+++ src/main/java/com/harri/projeto_santander_spring/ProjetoSantanderSpringApplication.java
@@ -0,0 +1,13 @@
+package com.harri.projeto_santander_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSantanderSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSantanderSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harri/projeto_santander_spring/ProjetoSantanderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harri.projeto_santander_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSantanderSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
