e825c94572307f05762693395870ad2c9eeec492
8harrison <monteiro4100@gmail.com>
2024-06-22 22:17:58 -0300

    Implementação da camada de dominio

---
+++ src/main/java/com/harri/projeto_santander_spring/domain/model/Card.java
@@ -0,0 +1,42 @@
+package com.harri.projeto_santander_spring.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(scale = 13, precision = 2, name = "available_limit")
+    private BigDecimal limit;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public BigDecimal getLimit() {
+        return limit;
+    }
+
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/com/harri/projeto_santander_spring/domain/model/News.java
@@ -0,0 +1,8 @@
+package com.harri.projeto_santander_spring.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class News extends BaseItem {
+
+}
